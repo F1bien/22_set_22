59cbf225f2574faeb45fc59d52d0a2a121c59c4a
Fibien <fabian.marello@gmail.com>
2022-10-01 16:43:32 +0200

    add comment

--- es_4/main.c
+++ es_4/main.c
@@ -1,27 +1,35 @@
+/*
+	Un file di testo contiene un certo numero di parole.
+	Ogni parola (a eccezione dell ultima)
+	e separata da quella successiva da un solo carattere di whitespace.
+*/
+
 #include <stdio.h>
 
-
-
-int CountChar( char fileN[] ) {
+// funzione per contrare le parole al interno di un file
+int CountChar( char fileN[] ) { // come argomento viene passato solo il nome del file
 	
 	int count = 0;
 	char buffer [100];
+	// apertura del file in read mod
 	FILE * fileP = fopen(fileN, "r");
-	if ( fileP== NULL ) {
+	if ( fileP== NULL ) { // controlo se il file si e aperto
 		printf("\nERRORE imposibile aprire file");
 		count = -1;
 	}
-	else {
+
+	else { // file aperto coretamente
 		//printf("\nfile aperto\n");
-		while ( !feof(fileP) ) {
-			count += fscanf(fileP,"%s", buffer);
+		while ( !feof(fileP) ) { // ciclo finche il file non finisce
+			count += fscanf(fileP,"%s", buffer); // incremanta count ogni volta che legge una
+												 // parola separate da uno spazio o newline
 			//printf("%d %s\n", count, buffer);
 		
 		}
 	}
 	
-	fclose(fileP);
-	return count+1;
+	fclose(fileP); // chiusura del file
+	return count+1; // la funzione restituisce il numero di parole
 }
 
 
@@ -30,10 +38,10 @@ int main () {
 	
 	printf("inserire nome file ");
 	char fileN[100];
-	scanf("%s",fileN);
+	scanf("%s",fileN); // input nome del file da user 
 	int count = CountChar(fileN);
 	
-	printf("\n%d\n", count);
+	printf("\n%d\n", count); // stampa a video del numero di parole
 	
 	
 	return 0;
